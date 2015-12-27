20c76589f3a9025aafb724d9a190a5c168ff491f
Vitali Fokin <vitali.fokin@gmail.com>
2015-12-28 00:12:20 +0300

    Fix path to bash_completion on osx

--- bash/bashrc.d/completion.bash
+++ bash/bashrc.d/completion.bash
@@ -41,8 +41,8 @@ complete -A function -A variable unset
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
 
 # Add tab completion for many Bash commands
-if which brew > /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
-  source "$(brew --prefix)/share/bash-completion/bash_completion";
+if which brew > /dev/null && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+  source "$(brew --prefix)/etc/bash_completion";
 elif [ -f /etc/bash_completion ]; then
   source /etc/bash_completion;
 fi;
