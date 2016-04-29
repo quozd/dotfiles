8b57c7e9786d820b2a5794216560f2c8174965bd
Vitali Fokin <vitali.fokin@gmail.com>
2016-04-29 14:04:59 +0300

    bash: fix completion for linux

--- bash/bashrc.d/completion.bash
+++ bash/bashrc.d/completion.bash
@@ -43,6 +43,8 @@ complete -A function -A variable unset
 # Add tab completion for many Bash commands
 if which brew &> /dev/null && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
   source "$(brew --prefix)/etc/bash_completion";
+elif [ -f /usr/share/bash-completion/bash_completion ]; then
+  source /usr/share/bash-completion/bash_completion
 elif [ -f /etc/bash_completion ]; then
   source /etc/bash_completion;
 fi;
