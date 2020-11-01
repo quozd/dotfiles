d154465e81d24481497dfef01807506b975456b6
Vitali Fokin <vitali.fokin@gmail.com>
2020-11-01 14:00:50 +0300

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,12 @@ set -x LANG en_US.UTF-8
 set -x LC_LANG en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
-abbr brwe="brew"
-abbr mux="tmuxinator"
+abbr brwe "brew"
+abbr mux "tmuxinator"
+abbr dc "docker-compose"
+
+set PATH /usr/local/bin $PATH
 
 # Local bash config
 test -e $HOME/.fishrc_local; and . $HOME/.fishrc_local
 
-# Iterm2 shell integration
-test -e $HOME/.iterm2_shell_integration.fish; and . $HOME/.iterm2_shell_integration.fish
